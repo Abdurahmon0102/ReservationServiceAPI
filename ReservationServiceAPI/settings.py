2c659b334bdbfa28a20d831578a50bb392333542
abdurahmon0945 <abdurahmonturgunov82@gmail.com>
2024-11-22 15:12:02 +0500

    changed settings to deploy

--- ReservationServiceAPI/settings.py
+++ ReservationServiceAPI/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'ReservationServiceAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     #     'NAME':  'ReservationServiceAPI',
     #     'USER': 'postgres',
     #     'PASSWORD': 'abdurahmon2009',
